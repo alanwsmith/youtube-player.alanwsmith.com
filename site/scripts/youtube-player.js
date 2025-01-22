98dd5912362477a1494d99b3273e0bd6c988cf7e
Alan Smith <commit@alanwsmith.com>
2025-01-22 01:20:19 -0500

    checking for buffering to help with pause delays

--- site/scripts/youtube-player.js
+++ site/scripts/youtube-player.js
@@ -4,6 +4,20 @@ class YouTubePlayer extends HTMLElement {
   static instances = {}
   static timeout = null
 
+  static handleBuffering(instance) {
+    // this is here to help prevent the
+    // pause from flowing through and triggering
+    // the style changes when scrolling
+    this.activeInstance = instance.uuid
+    if (instance.uuid == this.activeInstance) {
+      if (this.timeout !== null) {
+        clearTimeout(this.timeout)
+        this.timeout = null
+      }
+      document.body.dataset.youtubePlayerState = 'playing'
+    }
+  }
+
   static handleEnded(instance) {
     // console.log(`handleEnded: ${instance.uuid}`)
     this.activeInstance = instance.uuid
@@ -37,7 +51,7 @@ class YouTubePlayer extends HTMLElement {
             this.instances[uuid].doRemoveFade()
           }
         }
-      }, 900)
+      }, 200)
     }
   }
 
@@ -87,10 +101,14 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
     <div id="player"></div>
   </div>
   <div class="title"></div>
+  <!--
+  <div class="click-catcher"></div>
   <div class="yt-logo"></div>
+  -->
 </div>
 <div class="buttons">
   <button aria-label="play" class="play-button control-button"></button>
+  <button aria-label="mute" class="mute-button control-button"></button>
 </div>
 `
     const contents = 
@@ -99,12 +117,14 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
     // NOTE: player isn't added here since the element
     // is changed when the iframe loads
     this.parts.background = this.shadowRoot.querySelector('.background')
-    this.parts.logo = this.shadowRoot.querySelector('.yt-logo')
+    // this.parts.logo = this.shadowRoot.querySelector('.yt-logo')
     this.parts.shader = this.shadowRoot.querySelector('.shader')
     this.parts.thumbnail = this.shadowRoot.querySelector('.thumbnail')
     this.parts.title = this.shadowRoot.querySelector('.title')
     this.parts.wrapper = this.shadowRoot.querySelector('.wrapper')
     this.parts.playButton = this.shadowRoot.querySelector('.play-button')
+    this.parts.muteButton = this.shadowRoot.querySelector('.mute-button')
+    this.parts.clickCatcher = this.shadowRoot.querySelector('.click-catcher')
   }
 
   addEventListeners() {
@@ -112,8 +132,17 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
     this.parts.background.addEventListener('click', (event) => {
       this.handleWrapperClick.call(this, event)
     })
+    // this.parts.background.addEventListener('mousedown', (event) => {
+    //   console.log(event)
+    // })
+    // this.parts.background.addEventListener('mouseup', (event) => {
+    //   console.log(event)
+    // })
     this.parts.playButton.addEventListener('click', (event) => {
       this.handlePlayButtonClick.call(this, event)
+    })
+    this.parts.wrapper.addEventListener('click', (event) => {
+      console.log('click')
     })
   }
 
@@ -155,10 +184,12 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   }
 
   doEnded() {
+    this.parts.muteButton.classList.remove('dark')
+    this.parts.playButton.classList.remove('dark')
     this.parts.playButton.classList.add('play-button')
     this.parts.playButton.classList.remove('pause-button')
     this.parts.thumbnail.classList.remove('hidden')
-    this.parts.logo.classList.remove('hidden')
+    // this.parts.logo.classList.remove('hidden')
     this.parts.background.classList.remove('playing')
     this.parts.background.classList.add('stopped')
     this.parts.background.classList.remove('faded')
@@ -167,10 +198,12 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   }
 
   doPlaying() {
+    this.parts.muteButton.classList.add('dark')
+    this.parts.playButton.classList.add('dark')
     this.parts.playButton.classList.remove('play-button')
     this.parts.playButton.classList.add('pause-button')
     this.parts.thumbnail.classList.add('hidden')
-    this.parts.logo.classList.add('hidden')
+    // this.parts.logo.classList.add('hidden')
     this.parts.background.classList.add('playing')
     this.parts.background.classList.remove('stopped')
     this.parts.background.classList.remove('faded')
@@ -183,6 +216,8 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   }
 
   doPauseAndFade() {
+    this.parts.muteButton.classList.add('dark')
+    this.parts.playButton.classList.add('dark')
     this.parts.playButton.classList.add('play-button')
     this.parts.playButton.classList.remove('pause-button')
     this.parts.background.classList.add('faded')
@@ -202,6 +237,8 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   }
 
   doPauseOnActivePlayer() {
+    this.parts.muteButton.classList.remove('dark')
+    this.parts.playButton.classList.remove('dark')
     this.parts.playButton.classList.add('play-button')
     this.parts.playButton.classList.remove('pause-button')
     this.parts.background.classList.remove('faded')
@@ -210,6 +247,8 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   }
 
   doRemoveFade() {
+    this.parts.muteButton.classList.remove('dark')
+    this.parts.playButton.classList.remove('dark')
     this.parts.background.classList.remove('faded')
     this.parts.background.classList.remove('playing')
     this.parts.background.classList.add('stopped')
@@ -274,18 +313,24 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   handlePlayerStateChange(event) {
     const playerState = event.target.getPlayerState()
     if (playerState == -1) {
+      console.log("STATUS: UNSTARTED")
       // N/A - here for reference
     } else if (playerState == YT.PlayerState.BUFFERING) {
-      // N/A - here for reference
+      console.log("STATUS: BUFFERING")
+      this.constructor.handleBuffering(this)
     } else if (playerState == YT.PlayerState.CUED) {
+      console.log("STATUS: CUED")
       // N/A - here for reference
     } else if (playerState == YT.PlayerState.ENDED) {
+      console.log("STATUS: ENDED")
       // TODO: Move the start time back to the original 
       // start time.
       this.constructor.handleEnded(this)
     } else if (playerState == YT.PlayerState.PAUSED) {
+      console.log("STATUS: PAUSED")
       this.constructor.handlePause(this)
     } else if (playerState == YT.PlayerState.PLAYING) {
+      console.log("STATUS: PLAYING")
       this.constructor.switchActivePlayer(this)
     }
   }
@@ -302,9 +347,9 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
     this.player.playVideo()
   }
 
-  handleYtLogoClick(event) {
-    this.player.playVideo()
-  }
+  // handleYtLogoClick(event) {
+  //   this.player.playVideo()
+  // }
 
   async init() {
     this.loadApi()
@@ -339,6 +384,9 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
     // The player can be added now that it's
     // been switched to the iframe
     this.parts.player = this.shadowRoot.querySelector('#player')
+    // window.addEventListener('click', () => {
+    //   console.log("asdf")
+    // })
   }
 
   loadApi() {
@@ -375,6 +423,16 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   position: relative;
   width: 3rem;
   height: 2rem;
+}
+.control-button:after {
+  background: ${this.colors["base-foreground"]};
+  content: "";
+  height: 100%;
+  left: 0;
+  margin: 0;
+  position: absolute;
+  top: 0;
+  width: 100%;
 }
 .hidden {
   opacity: 0;
@@ -474,41 +532,31 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   padding-bottom: 56.25%;
 }
 .pause-button:after {
-  background: ${this.colors["base-foreground"]};
-  content: "";
-  height: 100%;
-  left: 0;
-  margin: 0;
   mask-image: url("data:image/svg+xml;utf8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%3Csvg%20width%3D%2240px%22%20height%3D%2240px%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20color%3D%22%23000000%22%20stroke-width%3D%222%22%3E%3Cpath%20d%3D%22M6%2018.4V5.6C6%205.26863%206.26863%205%206.6%205H9.4C9.73137%205%2010%205.26863%2010%205.6V18.4C10%2018.7314%209.73137%2019%209.4%2019H6.6C6.26863%2019%206%2018.7314%206%2018.4Z%22%20fill%3D%22%23000000%22%20stroke%3D%22%23000000%22%20stroke-width%3D%222%22%3E%3C%2Fpath%3E%3Cpath%20d%3D%22M14%2018.4V5.6C14%205.26863%2014.2686%205%2014.6%205H17.4C17.7314%205%2018%205.26863%2018%205.6V18.4C18%2018.7314%2017.7314%2019%2017.4%2019H14.6C14.2686%2019%2014%2018.7314%2014%2018.4Z%22%20fill%3D%22%23000000%22%20stroke%3D%22%23000000%22%20stroke-width%3D%222%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E");
   mask-position: center;
   mask-repeat: no-repeat;
   mask-size: contain;
-  position: absolute;
-  top: 0;
-  width: 100%;
 }
 .play-button:after {
-  background: ${this.colors["base-foreground"]};
-  content: "";
-  height: 100%;
-  left: 0;
-  margin: 0;
   mask-image: url("data:image/svg+xml;utf8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%3Csvg%20width%3D%2240px%22%20height%3D%2240px%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20color%3D%22%23000000%22%20stroke-width%3D%222%22%3E%3Cpath%20d%3D%22M6.90588%204.53682C6.50592%204.2998%206%204.58808%206%205.05299V18.947C6%2019.4119%206.50592%2019.7002%206.90588%2019.4632L18.629%2012.5162C19.0211%2012.2838%2019.0211%2011.7162%2018.629%2011.4838L6.90588%204.53682Z%22%20fill%3D%22%23000000%22%20stroke%3D%22%23000000%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E");
   mask-position: center;
   mask-repeat: no-repeat;
   mask-size: contain;
-  position: absolute;
-  top: 0;
-  width: 100%;
+}
+.mute-button:after {
+  mask-image: url("data:image/svg+xml;utf8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%3Csvg%20width%3D%2240px%22%20height%3D%2240px%22%20viewBox%3D%220%200%2024%2024%22%20stroke-width%3D%222%22%20fill%3D%22none%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20color%3D%22%23000000%22%3E%3Cpath%20fill-rule%3D%22evenodd%22%20clip-rule%3D%22evenodd%22%20d%3D%22M16.9697%206.96967C17.2626%206.67678%2017.7374%206.67678%2018.0303%206.96967L17.5%207.5C18.0303%206.96967%2018.0306%206.96989%2018.0308%206.97012L18.0313%206.9706L18.0323%206.97168L18.0349%206.97426L18.0416%206.98113L18.0613%207.00165C18.0771%207.01833%2018.0982%207.04101%2018.1237%207.06959C18.1747%207.1267%2018.2439%207.20756%2018.325%207.31121C18.487%207.51816%2018.6983%207.8181%2018.9084%208.20336C19.3286%208.97364%2019.75%2010.0966%2019.75%2011.5C19.75%2012.9034%2019.3286%2014.0264%2018.9084%2014.7966C18.6983%2015.1819%2018.487%2015.4818%2018.325%2015.6888C18.2439%2015.7924%2018.1747%2015.8733%2018.1237%2015.9304C18.0982%2015.959%2018.0771%2015.9817%2018.0613%2015.9984L18.0416%2016.0189L18.0349%2016.0257L18.0323%2016.0283L18.0313%2016.0294L18.0308%2016.0299C18.0306%2016.0301%2018.0303%2016.0303%2017.5207%2015.5207L18.0303%2016.0303C17.7374%2016.3232%2017.2626%2016.3232%2016.9697%2016.0303C16.6776%2015.7383%2016.6768%2015.2654%2016.9671%2014.9723C16.9679%2014.9714%2016.9688%2014.9705%2016.9697%2014.9697L17.5%2015.5C16.9697%2014.9697%2016.9695%2014.9699%2016.9693%2014.9701L16.9689%2014.9705L16.9682%2014.9711L16.9673%2014.9721L16.9724%2014.9667C16.9786%2014.9602%2016.9897%2014.9482%2017.0052%2014.9309C17.0362%2014.8962%2017.0842%2014.8404%2017.1437%2014.7643C17.263%2014.6119%2017.4267%2014.3806%2017.5916%2014.0784C17.9214%2013.4736%2018.25%2012.5966%2018.25%2011.5C18.25%2010.4034%2017.9214%209.52636%2017.5916%208.92164C17.4267%208.6194%2017.263%208.38809%2017.1437%208.23567C17.0842%208.15963%2017.0362%208.10377%2017.0052%208.06908C16.9897%208.05176%2016.9786%208.03978%2016.9724%208.03326L16.9671%208.02774C16.6768%207.73464%2016.6776%207.2617%2016.9697%206.96967Z%22%20fill%3D%22%23000000%22%3E%3C%2Fpath%3E%3Cpath%20fill-rule%3D%22evenodd%22%20clip-rule%3D%22evenodd%22%20d%3D%22M19.9697%203.96967C20.2626%203.67678%2020.7374%203.67678%2021.0303%203.96967L20.5%204.5C21.0303%203.96967%2021.0306%203.96991%2021.0308%203.97017L21.0314%203.97072L21.0327%203.972L21.0359%203.97527L21.045%203.98462C21.0523%203.9921%2021.0619%204.00207%2021.0736%204.01451C21.0971%204.03939%2021.1292%204.0742%2021.1688%204.11882C21.2478%204.20802%2021.3566%204.33662%2021.4851%204.50365C21.7419%204.83749%2022.0786%205.32653%2022.4137%205.96319C23.0845%207.23773%2023.75%209.10689%2023.75%2011.5C23.75%2013.8931%2023.0845%2015.7623%2022.4137%2017.0368C22.0786%2017.6735%2021.7419%2018.1625%2021.4851%2018.4963C21.3566%2018.6634%2021.2478%2018.792%2021.1688%2018.8812C21.1292%2018.9258%2021.0971%2018.9606%2021.0736%2018.9855C21.0619%2018.9979%2021.0523%2019.0079%2021.045%2019.0154L21.0359%2019.0247L21.0327%2019.028L21.0314%2019.0293L21.0308%2019.0298C21.0306%2019.0301%2021.0303%2019.0303%2020.5%2018.5L21.0303%2019.0303C20.7374%2019.3232%2020.2626%2019.3232%2019.9697%2019.0303C19.6771%2018.7378%2019.6768%2018.2636%2019.9687%2017.9706C19.9688%2017.9706%2019.9689%2017.9705%2019.969%2017.9704L19.9689%2017.9705L19.9687%2017.9706L19.9683%2017.9711L19.9678%2017.9716C19.9679%2017.9714%2019.9684%2017.9709%2019.9693%2017.97L19.9825%2017.9562C19.9957%2017.9422%2020.0173%2017.9189%2020.0461%2017.8864C20.1038%2017.8213%2020.1903%2017.7194%2020.2962%2017.5818C20.5081%2017.3062%2020.7964%2016.889%2021.0863%2016.3382C21.6655%2015.2377%2022.25%2013.6069%2022.25%2011.5C22.25%209.39311%2021.6655%207.76227%2021.0863%206.66181C20.7964%206.11097%2020.5081%205.69376%2020.2962%205.41822C20.1903%205.28057%2020.1038%205.17869%2020.0461%205.1136C20.0173%205.08107%2019.9957%205.05777%2019.9825%205.04384L19.9693%205.03C19.9683%205.02899%2019.9678%205.02845%2019.9677%205.02839L19.9683%205.02891L19.9689%205.02951L19.9692%205.02989C19.6768%204.73696%2019.6769%204.26242%2019.9697%203.96967ZM19.9677%205.02838C19.9677%205.02838%2019.9677%205.02838%2019.9677%205.02839L19.9677%205.02838Z%22%20fill%3D%22%23000000%22%3E%3C%2Fpath%3E%3Cpath%20fill-rule%3D%22evenodd%22%20clip-rule%3D%22evenodd%22%20d%3D%22M12.0367%203.3964C13.2002%202.62923%2014.75%203.46373%2014.75%204.85741V19.1431C14.75%2020.5368%2013.2002%2021.3713%2012.0367%2020.6041L6.03762%2016.6487C5.99677%2016.6218%205.94892%2016.6074%205.9%2016.6074H3C1.48122%2016.6074%200.25%2015.3762%200.25%2013.8574V10.1431C0.25%208.62434%201.48122%207.39313%203%207.39313H5.9C5.94892%207.39313%205.99677%207.37877%206.03762%207.35184L12.0367%203.3964Z%22%20fill%3D%22%23000000%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E");
+  mask-position: center;
+  mask-repeat: no-repeat;
+  mask-size: contain;
 }
 #player {
-  transition: all 0.7s ease-out;
+  transition: all 0.6s ease-out;
 }
 .wrapper, .shader, .thumbnail {
-  transition: opacity 0.7s ease-in;
+  transition: opacity 0.6s ease-in;
 }
 .background {
-  transition: border 0.7s ease-in;
+  transition: border 0.6s ease-in;
 }
 /*
 #player {
@@ -518,6 +566,7 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   width: 30%;
 }
 */
+/*
 .yt-logo {
   background-image: url("data:image/svg+xml;utf8,<svg id='Layer_1' data-name='Layer 1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 176 124'><defs><style>.cls-1 {fill: white;} .cls-2 {fill: red;}</style></defs><path class='cls-2' d='M172.32,19.36c-2.02-7.62-7.99-13.62-15.56-15.66C143.04,0,88,0,88,0c0,0-55.04,0-68.76,3.7-7.57,2.04-13.54,8.04-15.56,15.66C0,33.18,0,62,0,62c0,0,0,28.82,3.68,42.64,2.02,7.62,7.99,13.62,15.56,15.66,13.73,3.7,68.76,3.7,68.76,3.7,0,0,55.04,0,68.76-3.7,7.57-2.04,13.54-8.04,15.56-15.66,3.68-13.81,3.68-42.64,3.68-42.64,0,0,0-28.82-3.68-42.64Z'/><polygon class='cls-1' points='70 88.17 116 62 70 35.83 70 88.17'/></svg>");
   background-size: contain;
@@ -531,8 +580,9 @@ https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
   position: absolute;
   width: 3.5rem;
   z-index: 5;
-  transition: opacity 0.7s ease-in;
+  transition: opacity 0.6s ease-in;
 }
+*/
 @media (hover: hover) {
   .background:hover .title {
     color: white;
