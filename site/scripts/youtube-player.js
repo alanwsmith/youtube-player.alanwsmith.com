6223051ded7eefbd6f6928676af2077a4659f6cc
Alan Smith <commit@alanwsmith.com>
2025-01-21 23:55:21 -0500

    alphabitized and moved old stuff out

--- site/scripts/youtube-player.js
+++ site/scripts/youtube-player.js
@@ -69,6 +69,62 @@ class YouTubePlayer extends HTMLElement {
     }
   }
 
+  addContent() {
+    const template = 
+      this.ownerDocument.createElement('template')
+    template.innerHTML = `
+<div class="background stopped">
+  <div class="thumbnail">
+    <object type="image/jpg" data="https://i.ytimg.com/vi/${this.attrs.video}/maxresdefault.jpg" aria-label="Video thumbnail image">
+      <img src="https://i.ytimg.com/vi/${this.attrs.video}/hqdefault.jpg" aria-label="Video thumbnail image" />
+      <!--
+https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
+      -->
+    </object>
+  </div>
+  <div class="shader hidden"></div>
+  <div class="wrapper hidden">
+    <div id="player"></div>
+  </div>
+  <div class="title"></div>
+  <div class="yt-logo"></div>
+</div>
+<div class="buttons">
+  <button aria-label="play" class="play-button control-button"></button>
+</div>
+`
+    const contents = 
+      template.content.cloneNode(true)
+    this.shadowRoot.append(contents)
+    // NOTE: player isn't added here since the element
+    // is changed when the iframe loads
+    this.parts.background = this.shadowRoot.querySelector('.background')
+    this.parts.logo = this.shadowRoot.querySelector('.yt-logo')
+    this.parts.shader = this.shadowRoot.querySelector('.shader')
+    this.parts.thumbnail = this.shadowRoot.querySelector('.thumbnail')
+    this.parts.title = this.shadowRoot.querySelector('.title')
+    this.parts.wrapper = this.shadowRoot.querySelector('.wrapper')
+    this.parts.playButton = this.shadowRoot.querySelector('.play-button')
+  }
+
+  addEventListeners() {
+    // const background = this.shadowRoot.querySelector(".background")
+    this.parts.background.addEventListener('click', (event) => {
+      this.handleWrapperClick.call(this, event)
+    })
+    this.parts.playButton.addEventListener('click', this)
+  }
+
+  connectedCallback() {
+    this.constructor.registerInstance(this)
+    this.getAttributes()
+    this.addContent()
+    this.addStyles()
+    this.getTitle()
+    this.addEventListeners()
+    this.init()
+  }
+
   constructor() {
     super()
     this.uuid = self.crypto.randomUUID()
@@ -118,7 +174,23 @@ class YouTubePlayer extends HTMLElement {
       this.parts.shader.classList.remove('dark-shader-over-background')
       this.parts.shader.classList.remove('hidden')
     }, 3000)
+  }
 
+  doPauseAndFade() {
+    this.parts.background.classList.add('faded')
+    this.parts.background.classList.remove('playing')
+    this.parts.background.classList.remove('stopped')
+    if (this.player.getPlayerState() === 1 || this.player.getPlayerState() === 2) {
+      this.parts.shader.classList.remove('dark-shader-over-background')
+      this.parts.shader.classList.remove('hidden')
+      this.parts.player.classList.add('dark')
+    } else {
+      this.parts.shader.classList.add('dark-shader-over-background')
+      this.parts.shader.classList.remove('hidden')
+    }
+    if (this.player.getPlayerState() === 1) {
+      this.player.pauseVideo()
+    }
   }
 
   doPauseOnActivePlayer() {
@@ -127,21 +199,16 @@ class YouTubePlayer extends HTMLElement {
     this.parts.background.classList.add('stopped')
   }
 
-  addEventListeners() {
-    const background = this.shadowRoot.querySelector(".background")
-    background.addEventListener("click", (event) => {
-      this.handleWrapperClick.call(this, event)
-    })
-  }
-
-  connectedCallback() {
-    this.constructor.registerInstance(this)
-    this.getAttributes()
-    this.addContent()
-    this.addStyles()
-    this.getTitle()
-    this.addEventListeners()
-    this.init()
+  doRemoveFade() {
+    this.parts.background.classList.remove('faded')
+    this.parts.background.classList.remove('playing')
+    this.parts.background.classList.add('stopped')
+    if (this.player.getPlayerState() === 2) {
+      this.parts.player.classList.remove('dark')
+    } else {
+      this.parts.shader.classList.add('hidden')
+      this.parts.shader.classList.remove('dark-shader-over-background')
+    }
   }
 
   getAttributes() {
@@ -167,201 +234,6 @@ class YouTubePlayer extends HTMLElement {
     }
   }
 
-  // addButtons(player) {
-  //   this.playButton = document.createElement('button')
-  //   this.playButton.innerHTML = '&nbsp;'
-  //   this.playButton.classList.add('youtube-button')
-  //   this.playButton.classList.add('youtube-play-button')
-  //   this.playButton.classList.add('youtube-play-icon')
-  //   this.playButton.addEventListener('click', (event) => {
-  //     this.doPlayPause.call(this, event, this.player)
-  //   })
-  //   this.stopButton = document.createElement('button')
-  //   this.stopButton.innerHTML = 'stop'
-  //   this.stopButton.classList.add('youtube-button')
-  //   this.stopButton.classList.add('youtube-stop-button')
-  //   this.stopButton.addEventListener('click', (event) => {
-  //     this.doStop.call(this, event, this.player)
-  //   })
-  //   this.buttonWrapper.appendChild(this.playButton)
-  //   this.buttonWrapper.appendChild(this.stopButton)
-  //   this.addSpeedButtons(player)
-  //   this.muteButton = document.createElement('button')
-  //   if (player.isMuted() === true) {
-  //     this.muteButton.innerHTML = 'unmute'
-  //   } else {
-  //     this.muteButton.innerHTML = 'mute'
-  //   }
-  //   this.muteButton.classList.add('youtube-button')
-  //   this.muteButton.classList.add('youtube-mute-button')
-  //   this.muteButton.addEventListener('click', (event) => {
-  //     this.doMuteUnmute.call(this, event, this.player)
-  //   })
-  //   this.buttonWrapper.appendChild(this.muteButton)
-  //   this.ytLogo.style.visibility = 'visible'
-  // }
-  //
-
-  addFastForwardButton() {
-    this.fastForwardButton = document.createElement("button")
-    this.fastForwardButton.classList.add("youtube-rewind-button")
-    this.fastForwardButton.innerHTML = ">>"
-    this.fastForwardButton.addEventListener("click", (event) => {
-      this.handleFastForwardButtonClick.call(this, event)
-    })
-    this.buttonWrapper.appendChild(this.fastForwardButton)
-  }
-
-  addMuteButton() {
-    this.muteButton = document.createElement("button")
-    this.muteButton.classList.add("youtube-mute-button")
-    this.muteButton.innerHTML = "mute"
-    this.muteButton.addEventListener("click", (event) => {
-      this.handleMuteButtonClick.call(this, event)
-    })
-    this.buttonWrapper.appendChild(this.muteButton)
-  } 
-
-  addRewindButton() {
-    this.rewindButton = document.createElement("button")
-    this.rewindButton.classList.add("youtube-rewind-button")
-    this.rewindButton.innerHTML = "<<"
-    this.rewindButton.addEventListener("click", (event) => {
-      this.handleRewindButtonClick.call(this, event)
-    })
-    this.buttonWrapper.appendChild(this.rewindButton)
-  }
-
-  //addSpeedButtons(player) {
-  //  this.speedButtons = []
-  //  // TODO: remove the strings and just add a
-  //  // data-speed attribute that can be used
-  //  // for selecting
-  //  //
-  //  // TODO: Add a data-speed to the main
-  //  // player that can be used for selection too
-  //  const speeds = [
-  //    [1, '1'],
-  //    [1.5, '1-5'],
-  //    [2, '2'],
-  //    [2.5, '2-5'],
-  //    [3, '3'],
-  //    [4, '4']
-  //  ]
-  //  speeds.forEach((speed) => {
-  //    const speedButton = document.createElement('button')
-  //    speedButton.classList.add('youtube-button')
-  //    speedButton.classList.add('youtube-speed-button')
-  //    speedButton.innerHTML = `${speed[0]}x`
-  //    speedButton.dataset.speed = speed[0]
-  //    speedButton.addEventListener('click', (event) => {
-  //      this.adjustSpeed.call(this, event, player)
-  //    })
-  //    this.speedButtons.push(speedButton)
-  //  })
-  //  this.speedButtons.forEach((speedButton) => {
-  //    this.buttonWrapper.appendChild(speedButton)
-  //  })
-  //}
-
-  addPlaybackButtons() {
-    this.playbackRates.forEach((playbackRate) => {
-      const playbackButton = document.createElement('button')
-      playbackButton.classList.add('youtube-playback-button')
-      playbackButton.playbackRate = playbackRate
-      playbackButton.innerHTML = `${playbackRate}x`
-      playbackButton.addEventListener("click", (event) => {
-        this.handlePlaybackButtonClick.call(this, event)
-      })
-      this.buttonWrapper.appendChild(playbackButton)
-    })
-  }
-
-  // adjustSpeed(event, player) {
-  //   const speed = parseFloat(event.target.dataset.speed)
-  //   player.setPlaybackRate(speed)
-  // }
-
-  // applyInit() {
-  //   this.addButtons(this.player)
-  //   this.videoWrapper.addEventListener('click', () => {
-  //     this.player.playVideo()
-  //   })
-  // }
-
-  // buildStructure() {
-  //   this.videoWrapper = document.createElement('div')
-  //   this.videoWrapper.classList.add('youtube-video-wrapper')
-  //   this.appendChild(this.videoWrapper)
-  //   this.videoEl = document.createElement('div')
-  //   this.videoWrapper.appendChild(this.videoEl)
-  //   this.videoWrapper.style.backgroundImage = `url("https://i.ytimg.com/vi/${this.videoId}/maxresdefault.jpg")`
-  //   this.videoWrapper.style.backgroundSize = 'cover'
-  //   this.videoWrapper.style.backgroundPosition = 'center'
-  //   this.videoWrapper.style.backgroundRepeat = 'no-repeat'
-  //   this.buttonWrapper = document.createElement('div')
-  //   this.buttonWrapper.classList.add('youtube-button-wrapper')
-  //   this.appendChild(this.buttonWrapper)
-  //   this.ytLogo = document.createElement('div')
-  //   this.ytLogo.addEventListener("click", (event) => {
-  //     this.handleYtLogoClick.call(this, event)
-  //   })
-  //   this.ytLogo.classList.add('youtube-logo')
-  //   this.videoWrapper.appendChild(this.ytLogo)
-  // }
-
-  // doMuteUnmute(event, player) {
-  //   if (player.isMuted() === true) {
-  //     player.unMute()
-  //     this.muteButton.innerHTML = 'mute'
-  //   } else {
-  //     player.mute()
-  //     this.muteButton.innerHTML = 'unmute'
-  //   }
-  // }
-
-  // doPlayPause(event, player) {
-  //   const buttonEl = event.target
-  //   const playerState = player.getPlayerState()
-  //   // The docs don't list a YT.PlayerState.UNSTARTED
-  //   // flag so using the explicit `-1` instead
-  //   if (
-  //     playerState == -1 ||
-  //     playerState == YT.PlayerState.ENDED ||
-  //     playerState == YT.PlayerState.PAUSED ||
-  //     playerState == YT.PlayerState.BUFFERING ||
-  //     playerState == YT.PlayerState.CUED
-  //   ) {
-  //     player.playVideo()
-  //     // this.playButton.innerHTML = 'pause'
-  //     // TODO: Figure out how to shift focus to
-  //     // the player so keyboard controls work
-  //   } else {
-  //     player.pauseVideo()
-  //     // this.playButton.innerHTML = 'play'
-  //     // TODO: adjust this so it doesn't flash
-  //     // play when clicking to different parts
-  //     // of the video
-  //   }
-  // }
-
-  // doStop(event, player) {
-  //   // this.playButton.innerHTML = 'play'
-  //   this.player.g.style.visibility = 'hidden'
-  //   this.ytLogo.style.visibility = 'visible'
-  //   player.stopVideo()
-  // }
-
-  // getPlaybackRates() {
-  //   this.playbackRates = []
-  //   let targetRates = [0.5, 1, 1.5, 2, 3]
-  //   this.player.getAvailablePlaybackRates().forEach((playbackRate) => {
-  //     if (targetRates.includes(playbackRate)) {
-  //       this.playbackRates.push(playbackRate)
-  //     }
-  //   })
-  // }
-
   handleFastForwardButtonClick(event) {
     this.player.seekTo(
       Math.min(
@@ -379,23 +251,6 @@ class YouTubePlayer extends HTMLElement {
       this.muteButton.innerHTML = "unmute"
       this.player.mute()
     }
-  }
-
-  handlePlaybackButtonClick(event) {
-    if (this.player.getPlayerState() === 1) {
-      if (this.player.getPlaybackRate() === event.target.playbackRate) {
-        this.player.pauseVideo()
-      } else {
-        this.player.setPlaybackRate(event.target.playbackRate)
-      }
-    } else {
-      this.player.setPlaybackRate(event.target.playbackRate)
-      this.player.playVideo()
-    }
-  }
-
-  handlePlaybackRateChange(event) {
-    this.updateButtonStyles()
   }
 
   handlePlayerStateChange(event) {
@@ -453,30 +308,19 @@ class YouTubePlayer extends HTMLElement {
           onStateChange: (event) => {
             this.handlePlayerStateChange.call(this, event)
           },
-          onPlaybackRateChange: (event) => {
-            this.handlePlaybackRateChange.call(this, event)
-          }
+          // onPlaybackRateChange: (event) => {
+          //   this.handlePlaybackRateChange.call(this, event)
+          // }
         },
       })
     }).then((value) => {
       return value
     })
     // TODO: Figure out how to handle errors here.
-
+    //
     // The player can be added now that it's
     // been switched to the iframe
     this.parts.player = this.shadowRoot.querySelector('#player')
-
-    // this.addRewindButton()
-    // this.getPlaybackRates()
-    // this.addPlaybackButtons()
-    // this.addFastForwardButton()
-    // this.addMuteButton()
-    // this.ytLogo.style.visibility = "visible"
-    // this.videoWrapper.addEventListener("click", (event) => {
-    //   this.handleVideoWrapperClick.call(this, event)
-    // })
-
   }
 
   loadApi() {
@@ -495,21 +339,6 @@ class YouTubePlayer extends HTMLElement {
       }
       el.onerror = rej
       this.append(el)
-    })
-  }
-
-  updateButtonStyles() {
-    const playbackButtonNodes = document.querySelectorAll(".youtube-playback-button")
-    const playbackButtonEls = [...playbackButtonNodes]
-    playbackButtonEls.forEach((playbackButton) => {
-      if (
-        this.player.getPlaybackRate() === playbackButton.playbackRate
-        && this.player.getPlayerState() === 1
-      ) {
-        playbackButton.classList.add("youtube-active-rate")
-      } else {
-        playbackButton.classList.remove("youtube-active-rate")
-      }
     })
   }
 
@@ -688,79 +517,9 @@ class YouTubePlayer extends HTMLElement {
   .play-button:hover:after {
     background: ${this.colors['hover-foreground']};
   }
-}
-`
+}`
     );
     this.shadowRoot.adoptedStyleSheets.push(styles);
-  }
-
-  addContent() {
-    const template = 
-      this.ownerDocument.createElement('template')
-    template.innerHTML = `
-<div class="background stopped">
-  <div class="thumbnail">
-    <object type="image/jpg" data="https://i.ytimg.com/vi/${this.attrs.video}/maxresdefault.jpg" aria-label="Video thumbnail image">
-      <img src="https://i.ytimg.com/vi/${this.attrs.video}/hqdefault.jpg" aria-label="Video thumbnail image" />
-
-      <!--
-https://i.ytimg.com/vi/Cz8cbwR_6ms/hqdefault.jpg
-      -->
-
-    </object>
-  </div>
-  <div class="shader hidden"></div>
-  <div class="wrapper hidden">
-    <div id="player"></div>
-  </div>
-  <div class="title"></div>
-  <div class="yt-logo"></div>
-</div>
-<div class="buttons">
-  <button aria-label="play" class="play-button control-button"></button>
-</div>
-`
-    const contents = 
-      template.content.cloneNode(true)
-    this.shadowRoot.append(contents)
-    // NOTE: player isn't added here since the element
-    // is changed when the iframe loads
-    this.parts.background = this.shadowRoot.querySelector('.background')
-    this.parts.logo = this.shadowRoot.querySelector('.yt-logo')
-    this.parts.shader = this.shadowRoot.querySelector('.shader')
-    this.parts.thumbnail = this.shadowRoot.querySelector('.thumbnail')
-    this.parts.title = this.shadowRoot.querySelector('.title')
-    this.parts.wrapper = this.shadowRoot.querySelector('.wrapper')
-  }
-
-  doPauseAndFade() {
-    this.parts.background.classList.add('faded')
-    this.parts.background.classList.remove('playing')
-    this.parts.background.classList.remove('stopped')
-    if (this.player.getPlayerState() === 1 || this.player.getPlayerState() === 2) {
-      this.parts.shader.classList.remove('dark-shader-over-background')
-      this.parts.shader.classList.remove('hidden')
-      this.parts.player.classList.add('dark')
-
-    } else {
-      this.parts.shader.classList.add('dark-shader-over-background')
-      this.parts.shader.classList.remove('hidden')
-    }
-    if (this.player.getPlayerState() === 1) {
-      this.player.pauseVideo()
-    }
-  }
-
-  doRemoveFade() {
-    this.parts.background.classList.remove('faded')
-    this.parts.background.classList.remove('playing')
-    this.parts.background.classList.add('stopped')
-    if (this.player.getPlayerState() === 2) {
-      this.parts.player.classList.remove('dark')
-    } else {
-      this.parts.shader.classList.add('hidden')
-      this.parts.shader.classList.remove('dark-shader-over-background')
-    }
   }
 }
 
